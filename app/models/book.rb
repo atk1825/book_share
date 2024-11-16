b92c9ce0c30b00a6345322c2c33a51081b50e632
atk1825 <akkun1825@yahoo.co.jp>
2024-11-16 14:57:57 +0900

    必要事項の作成

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,6 @@
 class Book < ApplicationRecord
   has_one_attached :image
+  belongs_to :user
 
   validates :title, :author, :publisher, :overview, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'books#index'
-  resources :books, only: [:index, :new, :create, :show]
-
+  resources :books, only: %i[index new create show]
+  resources :users, only: [:index]
 end
